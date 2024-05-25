12b01ef8d649c0f8f6015a556aac556810754f7e
Emil <emil88pl@gmail.com>
2024-05-25 13:19:30 +0100

    super keyword

--- 01 All in one/index.js
+++ 01 All in one/index.js
@@ -1449,7 +1449,7 @@
 
 // 1
 // function Product(name, price) {
-    
+
 //     this.name = name;
 //     this.price = price;
 
@@ -1629,8 +1629,86 @@
 // console.log(`Rabbit is alive: ${howk1.alive}`)
 
 
+// super = keyword is used in classes to call the constructor or 
+//         access the properties and methods of a parent (superclass)
+//         this = this object
+//         super = the parent
 
 
+
+// class Animal {
+
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     move(speed) {
+//         console.log(`The ${this.name} moves at a speed of ${speed} mph`)
+//     }
+
+// }
+
+// class Dog extends Animal {
+
+//     constructor(name, age, runSpeed) {
+//         super(name, age);
+//         this.runSpeed = runSpeed;
+//     }
+
+//     run() {
+//         console.log(`This ${this.name} can run`);
+//         super.move(this.runSpeed);
+//     }
+
+// }
+
+// class Fish extends Animal {
+
+//     constructor(name, age, swimSpeed) {
+//         super(name, age);
+//         this.runSpeed = swimSpeed;
+//     }
+
+//     swim() {
+//         console.log(`This ${this.name} can swim`);
+//         super.move(this.runSpeed);
+//     }
+// }
+
+// class Stork extends Animal {
+
+//     constructor(name, age, flySpeed) {
+//         super(name, age);
+//         this.runSpeed = flySpeed;
+//     }
+
+//     fly() {
+//         console.log(`This ${this.name} can fly`);
+//         super.move(this.runSpeed);
+//     }
+// }
+
+
+// const dog1 = new Dog("Rex", 13, 54);
+// const fish1 = new Fish("Nemo", 3, 13);
+// const strk1 = new Stork("RedPik", 77, 120);
+
+// console.log(`Name: ${dog1.name}`);
+// console.log(`Age: ${dog1.age}`);
+// console.log(`Speed: ${dog1.runSpeed}`);
+// dog1.run();
+
+// console.log(`Name: ${fish1.name}`);
+// console.log(`Age: ${fish1.age}`);
+// console.log(`Speed: ${fish1.runSpeed}`);
+// fish1.swim();
+
+
+// console.log(`Name: ${strk1.name}`);
+// console.log(`Age: ${strk1.age}`);
+// console.log(`Speed: ${strk1.runSpeed}`);
+// strk1.fly();
 
 
 
