496ff5b28e0afa1f5a37d2f8119a3fe51e1da586
Emil <emil88pl@gmail.com>
2024-05-25 12:05:34 +0100

    static keyword

--- 01 All in one/index.js
+++ 01 All in one/index.js
@@ -1505,6 +1505,86 @@
 
 
 
+// static = keyword that defines properties or method that belong to a class itself rather
+//          to the objrcts created from that calss (class owns anything static, not hte objects)
+
+
+// class MathUtil {
+//     static PI = 3.14159;
+
+//     static getDiameter(radius) {
+//         return radius * 2;
+//     }
+
+//     static getCircumference(radius) {
+//         return 2 * this.PI * radius;
+//     }
+
+//     static getArea(radius) {
+//         return this.PI * radius * radius
+//     }
+// }
+
+// console.log(MathUtil.PI);
+// console.log(MathUtil.getDiameter(10));
+// console.log(MathUtil.getCircumference(12));
+// console.log(MathUtil.getArea(7));
+
+// class User{
+
+//     static userCount = 0;
+
+//     constructor(userName) {
+//         this.userName = userName;
+//         User.userCount++;
+//     }
+
+//     static getUserCount() {
+//         console.log(`There are ${User.userCount} users online`)
+//     }
+
+//     sayHello() {
+//         console.log(`Hello my user name is ${this.userName}`)
+//     }
+// }
+
+
+// const user1 = new User("Bond");
+// const user2 = new User("Jack");
+
+// user1.sayHello();
+// User.getUserCount();
+
+// console.log(`How many users was created: ${User.userCount}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
