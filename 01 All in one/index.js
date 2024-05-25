e74dba6854232b2443d79a0e91bb551e58f9c4e9
Emil <emil88pl@gmail.com>
2024-05-25 10:25:15 +0100

    constructor

--- 01 All in one/index.js
+++ 01 All in one/index.js
@@ -1414,13 +1414,32 @@
 // person1.sayFavFood();
 
 
+// constructor = special method  for defining the properties and methods of object
 
 
+// function Car(make, model, year, colour) {
+//         this.make = make;
+//         this.model = model;
+//         this.year = year;
+//         this.colour = colour;
+//         this.drive = function () {console.log(`You drive the ${this.make}`)}
+// }
 
+// const car1 = new Car("BMW", "4 Series", 2016, "Grey");
+// const car2 = new Car("VW", "Golf", 2024, "Blue")
 
+// console.log(car1.make);
+// console.log(car1.model);
+// console.log(car1.year);
+// console.log(car1.colour);
 
+// console.log(car2.make);
+// console.log(car2.model);
+// console.log(car2.year);
+// console.log(car2.colour);
 
-
+// car1.drive();
+// car2.drive();
 
 
 
