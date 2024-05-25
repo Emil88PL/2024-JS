8ed3a1968271fe604c3b0592b6135c8dc51ce846
Emil <emil88pl@gmail.com>
2024-05-25 18:15:24 +0100

    Nested objects

--- 01 All in one/index.js
+++ 01 All in one/index.js
@@ -1922,20 +1922,72 @@
 // displayPerson(Person1);
 // displayPerson(Person2);
 
+// Nested objects = Objects inside other Objects.
+//                  Allows you to represent more complex data strucures
+//                  Child Objects is enclosed by a Parent Object
+
+//                  Person{Address{}, ContactInfo{}}
+//                  ShoppingCart{Keyboard{}, Mause{}, Monitor{}}
+
+// ex 1
+// const person = {
+//     fullName: "James Bond",
+//     age: 30,
+//     isStudent: false,
+//     hobbies: ["Poker", "fishing", "iceSkating"],
+//     address: {
+//         street: "123 N",
+//         city: "London",
+//         country: "UK"
+//     }
+// }
+
+// console.log(person.fullName);
+// console.log(person.age);
+// console.log(person.isStudent);
 
 
+// console.log(person.hobbies[0]);
+// console.log(person.address.city);
 
+// for(const property in person.address) {
+//     console.log(`- ${person.address[property]}`)
+// }
 
+// ex 2
 
+// class Person {
 
+//     constructor(name, age, ...address) {
+//         this.name = name;
+//         this.age = age;
+//         this.address = new Address(...address);
+//     }
+// }
 
+// class Address {
 
+//     constructor(street, city, country) {
+//         this.street = street;
+//         this.city = city;
+//         this.country = country;
+//     }
+// }
 
+// const person1 = new Person("Bond", 30, "Bond Street", "London", "UK");
+// const person2 = new Person("Bruce", 70, "Bruce Street", "Coventry", "UK");
 
+// // console.log(person1.name);
+// // console.log(person1.age);
+// // console.log(person1.address);
+// // console.log(person2.name);
+// // console.log(person2.age);
+// // console.log(person2.address);
+// console.log(person1.address.street);
 
-
-
-
+// for(const property in person2.address) {
+//     console.log(`- ${person2.address[property]}`)
+// }
 
 
 
