fc852aad3f029632ff2a1913d60c7cafafe1fb2d
Emil <emil88pl@gmail.com>
2024-05-26 16:36:32 +0100

    Synchronous/Asynchronous

--- 01 All in one/index.js
+++ 01 All in one/index.js
@@ -2389,9 +2389,30 @@
 
 
 
+// Synchronous = Executes line by line consecutively in a sequential
+//               maner
+//               Code that waits for an operation to complete
 
+// Asynchronous = Allows multiple operations to be performed concurently
+//                without waiting
+//                Dosen't block the execution flow and allows program
+//                to continue (I/O operations, network requests, fetching data)
+//                Handle woth: Callback, Promises, Async/Await
 
+// function func1(callback) {
+//     setTimeout(() => {console.log("Task 1")
+//                       callback();
+//     }, 2000)
+  
+// }
 
+// function func2() {
+//     console.log("Task 2");
+//     console.log("Task 3");
+//     console.log("Task 4");
+// }
+
+// func1(func2);
 
 
 
