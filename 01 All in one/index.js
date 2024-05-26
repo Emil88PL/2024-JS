247b262cfcc6422304cd86a07bcff946b0dffeb8
Emil <emil88pl@gmail.com>
2024-05-26 09:23:59 +0100

    Shuffle an array - Fisher-Yates algorithm

--- 01 All in one/index.js
+++ 01 All in one/index.js
@@ -2119,10 +2119,19 @@
 
 
 
+// // Shuffle an array - Fisher-Yates algorithm
 
+// const cards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
 
+// shuffle(cards);
+// console.log(cards);
+// function shuffle(array) {
+//     for(let i = array.length -1; i > 0; i--) {
+//         const random = Math.floor(Math.random() * (i + 1));
 
-
+//         [array[i], array[random]] = [array[random], array[i]];
+//     }
+// }
 
 
 
