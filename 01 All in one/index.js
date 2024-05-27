b719ae95d6630399b8a5b260cd67c0c0c144c099
Emil <emil88pl@gmail.com>
2024-05-27 11:11:35 +0100

    eventListener click, mouseover, mouseout

--- 01 All in one/index.js
+++ 01 All in one/index.js
@@ -2741,6 +2741,86 @@
 
 
 
+// eventListener = Lidyrn for specific events to create interactive web pages
+//                 events: click, mouseover, mouseout
+//                 .addEventListener(event, callback);
+
+
+// const myBox = document.getElementById("myBox");
+// const myButton = document.getElementById("myButton");
+
+
+// // 1
+// myBox.addEventListener("click", event => {
+//     event.target.style.backgroundColor = "tomato";
+//     event.target.textContent = "OUCH! 😒"
+// })
+
+// myBox.addEventListener("mouseover", event => {
+//     event.target.style.backgroundColor = "yellow";
+//     event.target.textContent = "Don't do it! 😜"
+// })
+
+// myBox.addEventListener("mouseout", event => {
+//     event.target.style.backgroundColor = "lightgreen";
+//     event.target.textContent = "Click me! 😉"
+// })
+
+// // 2
+// myButton.addEventListener("click", event => {
+//     myBox.style.backgroundColor = "tomato";
+//     myBox.textContent = "OUCH! 😒"
+// })
+
+// myButton.addEventListener("mouseover", event => {
+//     myBox.style.backgroundColor = "yellow";
+//     myBox.textContent = "Don't do it! 😜"
+// })
+
+// myButton.addEventListener("mouseout", event => {
+//     myBox.style.backgroundColor = "lightgreen";
+//     myBox.textContent = "Click me! 😉"
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
